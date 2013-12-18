291d46057352f74e71af530808326cffe1e4e89e
Eric Schles <ericschles@gmail.com>
2013-12-18 06:08:05 -0500

    two hours of work

---
+++ ex13.rb
@@ -0,0 +1,8 @@
+first, second, thirds = ARGV
+
+puts "The script is called: #{$0}"
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{thirds}"
+
+
---
+++ testing.rb
@@ -0,0 +1,3 @@
+puts "this is a test"
+puts "really, I promise"
+#comment all the things
